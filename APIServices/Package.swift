14f9b0be1bb823b7202785d50e14aebf9783b13d
Yusuke Uchida <65172567+ewa1989@users.noreply.github.com>
2024-10-12 14:30:08 +0900

    Swift Concurrencyの設定を追加

--- APIServices/Package.swift
+++ APIServices/Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.5
+// swift-tools-version:5.10
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -21,9 +21,19 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "APIServices",
-            dependencies: ["Entities"]),
+            dependencies: ["Entities"],
+            swiftSettings: [
+                .enableExperimentalFeature("StrictConcurrency"),
+                .enableUpcomingFeature("GlobalConcurrency"),
+            ]
+        ),
         .testTarget(
             name: "APIServicesTests",
-            dependencies: ["APIServices"]),
+            dependencies: ["APIServices"],
+            swiftSettings: [
+                .enableExperimentalFeature("StrictConcurrency"),
+                .enableUpcomingFeature("GlobalConcurrency"),
+            ]
+        ),
     ]
 )
